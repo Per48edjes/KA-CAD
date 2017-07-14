a54e94dfa1ecc3d0958e32ecc5e64a8536dd4aa9
Ravi Dayabhai <ravidayabhai@Ravis-KA-MacBook-Pro.local>
2017-07-14 11:37:11 -0700

    Initial commit

---
+++ cache_parser.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+import json
+import pandas as pd
+
+
+### FUNCTION: Creates DataFrame from opened cache
+def df_creator(data_dict, df_merge_fields):
+
+    # Master output table
+    col_labels = ["site_url", "endpoint_category", "endpoint", "date", "value"]    
+    master = pd.DataFrame(columns = col_labels)
+
+    # HELPER FUNCTION: Makes mini-dataframes for appending to 'master'
+    def json_parser(list_of_jsons):
+
+        # Create empty dataframe for appending site info to
+        df = pd.DataFrame()
+
+        # Go through each site's data; create site-specific DataFrame
+        for metric in list_of_jsons:
+            endpoint = [key for key in metric.keys() if key != "meta"][0]
+            endpoint_category = metric["meta"]["request_parameters"]["endpoint_category"]
+            '''
+            Table structure:
+            SITE_NAME ... ENDPOINT CATEGORY ... ENDPOINT ... DATE ... VALUE
+            '''
+            data = [[site_name, endpoint_category, endpoint, d["date"], d[endpoint]] for d in metric[endpoint]]
+            site_df = pd.DataFrame(data, columns = col_labels)
+            df = df.append(site_df)    
+
+        return df
+
+    # Iterate through all of the sites
+    for k, v in data_dict.items():
+        jsons = v
+        site_name = k
+        master = master.append(json_parser(jsons)) 
+
+    # Add other (non-API request) fields to data
+    merged_master = master.merge(df_merge_fields, on="site_url")
+
+    # Transform 'merged_master' (to import into GDS)
+    master = merged_master.set_index(["group_site", "KA_initiative", "site_url", "site_name",
+        "endpoint_category", "date", "endpoint"])
+
+    # Move value fields to end, grouped by date
+    master = master.unstack(level=-1).reset_index()
+    master.columns = [' '.join(col).strip() if "value" not in col else
+            list(filter(lambda x: x != "value", col))[0] for col in master.columns.values]
+
+    print("Done making master DataFrame!")
+    return master
+
+
+### FUNCTION: Opens the cache and read data to dictionary (by site)
+def log_opener(file_path):
+    site_dict = {}     
+    last_site_scanned = None 
+
+    with open(file_path) as f:
+        for line in f:
+            try:
+                json_data = json.loads(line)  
+                site = json_data["meta"]["request"]["domain"] 
+                if site != last_site_scanned:
+                    site_dict[site] = [json_data]
+                else:
+                    site_dict[site].append(json_data)
+                last_site_scanned = site
+            except:
+                print("Skipped a line! Check error log for bad data.")
+
+    print("Done opening log!")
+    return site_dict
+
---
+++ pull_push.py
@@ -0,0 +1,197 @@
+#!/usr/bin/python3
+
+import gspread
+from urllib.request import urlopen
+import json
+import pandas as pd
+import numpy as np
+import datetime
+from dateutil.relativedelta import relativedelta
+import os
+from oauth2client.service_account import ServiceAccountCredentials
+import re
+import cache_parser as cp
+import shutil
+
+
+### OAuth2 credentialing and authentication; give gspread permissions 
+scopes = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('./client_secret.json', scopes=scopes)
+client = gspread.authorize(credentials)
+
+### Load input parameters for API calls 
+sheet = client.open("data_parameters").sheet1
+vals = sheet.get_all_values()
+headers = vals.pop(0)
+
+# Find latest cache, outfile
+file_re = re.compile(r'logs\/.*')
+cells = sheet.findall(file_re)
+cell_list = [(cell.row, cell.col) for cell in cells]
+latest_log_coords = max(cell_list, key=(lambda item: item[0]))
+
+file_re = re.compile(r'outfiles\/.*')
+cells = sheet.findall(file_re)
+cell_list = [(cell.row, cell.col) for cell in cells]
+latest_csv_coords = max(cell_list, key=(lambda item: item[0]))
+
+# Set up dataframe
+df = pd.DataFrame(vals, columns=headers)
+df.replace('',np.nan, inplace=True)
+
+# Get list of fields to merge before returning 'master'
+merge_fields = ["site_url", "site_name", "group_site", "KA_initiative"]
+
+# Get DataFrame with added fields to merge with 'master' before writing to CSV
+df_merge_fields = df.loc[:, merge_fields]
+
+# Get sites, endpoint categories, versions, endpoints, granularity
+parameters = {header:[params for params in df[header].dropna().values] for header in headers}
+df_map = df[["endpoint_category", "version"]].set_index(["endpoint_category"])
+endpoint_cat_version_map = df_map.to_dict()["version"]
+
+# Get last log, outfile filenames
+filename_last_log = parameters[headers[headers.index("log")]][-1] 
+filename_last_csv = parameters[headers[headers.index("outfile")]][-1] 
+
+
+def write_to_log(sites, endpoint_categories, endpoints):
+
+    # End date is the most recent month
+    today = datetime.date.today()
+    first = today.replace(day=1)
+    lastMonth = first - datetime.timedelta(days=1)
+    end_date = lastMonth.strftime("%Y-%m")
+
+    # Start date/log is dynamic depending on what's in the cache/what last
+    # cache is; MUST HAVE AT LEAST 'data_start.txt'!
+    if not os.path.isfile(filename_last_log) or os.stat(filename_last_log).st_size == 0:
+
+        # CODE HERE FOR RESETTING DATA IF NEW DIMENSIONS ADDED 
+
+        start_date_obj = lastMonth + relativedelta(months=-24)
+        log = filename_last_log
+        flag_new = False
+    else:
+        start_date_obj = lastMonth
+        now = datetime.datetime.now()
+        log = "logs/" + now.strftime("%Y-%m-%d_%H:%M") + ".txt"
+        flag_new = True
+
+    start_date = start_date_obj.strftime("%Y-%m")
+
+    # HELPER FUNCTION: Extract data, unless error (writes to error log)
+    def extractor(site, endpoint_category, version, endpoint, start_date, end_date, granularity="monthly"):
+
+        # API static input variables
+        API_key = "?api_key=08cdbb7d6a7be88a31ac85e85b219dcc"
+        domain = "https://api.similarweb.com/"
+        out_form = "&format=json"
+
+        # Call API; return data as JSON
+        API_link = domain + version + "/" + "website/" + site + "/" + endpoint_category+ "/" + endpoint + API_key + "&start_date=" + start_date + "&end_date=" + end_date + "&main_domain_only=false" + "&granularity=" + granularity + out_form  
+        
+        # Attempt the API request
+        try:
+            response = urlopen(API_link)
+            d = json.load(response)
+
+        except: 
+            d = {"meta": {"status": "404 ERROR"}}
+
+        # Add parameter information to the JSON request output
+        d["meta"]["request_parameters"] = {"site": site, "endpoint_category" :
+                endpoint_category, "endpoint": endpoint}
+
+        # Error checking
+        if d["meta"]["status"] != "Success":
+            # Writing error into error log 
+            print("ERROR HAPPENED!")
+            d["time"] = datetime.datime.now().strftime("%Y-%m-%d_%H:%M")
+            with open('logs/extraction_error_log.txt','a+') as outfile:
+                outfile.write(json.dumps(d["meta"]))
+                outfile.write("\n")
+                print("Wrote error to error log!")
+        else:   
+            return d
+
+    # Generate log data
+    all_json_data = []
+    for site in sites:
+        for endpoint_category in endpoint_categories:
+            for endpoint in endpoints:
+
+    # CODE HERE for preventing making requests if data already in log
+
+                extracted_data = extractor(site, endpoint_category, endpoint_cat_version_map[endpoint_category], endpoint, start_date, end_date) 
+                all_json_data.append(extracted_data)
+
+    # Write all log data to log file
+    with open(log, 'a+') as f:
+        for line in all_json_data:
+            f.write(json.dumps(line))
+            f.write("\n")
+
+    # Write NEW filename to 'data_parameters' under most recent log
+    if flag_new:
+        sheet.update_cell(latest_log_coords[0]+1, 1, log)
+        print("Log filename written to 'data_parameters'!")
+
+    print("Done writing to log!")
+    return log 
+        
+
+def write_to_outfile(df):
+    
+    # Checks last ouput CSV; MUST HAVE AT LEAST 'out_start.csv'!
+    if not os.path.isfile(filename_last_csv) or os.stat(filename_last_csv).st_size == 0:
+        csv = filename_last_csv
+        flag_new = False
+    else:
+        now = datetime.datetime.now()
+        csv = "outfiles/" + now.strftime("%Y-%m-%d_%H:%M") + ".csv"
+        flag_new = True
+
+        # Copy old file to new file    
+        shutil.copyfile(filename_last_csv, csv)
+
+    with open(csv, 'a+') as outfile:
+        # Writes the DataFrame to a CSV
+        if flag_new:
+            df.to_csv(outfile, header=False, index=False)
+        else:
+            df.to_csv(outfile, header=True, index=False)
+
+    # Write NEW filename to 'data_parameters' under most recent outfile
+    if flag_new:
+        sheet.update_cell(latest_csv_coords[0]+1, 2, csv)
+        print("Outfile filename written to 'data_parameters'!")
+   
+    # Overwrite last published data
+    shutil.copyfile(csv, "./data_source.csv")
+
+    print("Done writing to outfile!")
+    return
+
+
+### EXECUTION OF CODE
+if __name__ == "__main__":
+
+    # Governs whether API requests will be made
+
+    # CODE HERE FOR DETERMINING 'request_on' VALUE
+    # Things to check: last month of data, if fields or endpoints were added
+
+    requests_on = False
+    if requests_on:
+        log_file = write_to_log(parameters["site_url"], parameters["endpoint_category"], parameters["endpoint"]) 
+    else:
+        log_file = filename_last_log
+
+    # Governs if log is written to an outfile
+    log_to_out_on = False
+    if log_to_out_on:
+        df = cp.df_creator(cp.log_opener(log_file), df_merge_fields)
+        print(df.head(15))
+        write_to_outfile(df)
+
